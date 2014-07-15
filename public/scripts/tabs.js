f2496946adcede1657dacc6f9e0d7f0fc52e9edf
Eugene <agnivalent@gmail.com>
2014-07-15 06:28:22 +0400

    premerge

--- public/scripts/tabs.js
+++ public/scripts/tabs.js
@@ -1,16 +1,17 @@
 var changeTab = function(elem) {
-	$("#tabbar .active").removeClass('active');
+	var tabbar = $(elem).closest('.tabbar');
+	tabbar.find(".active").removeClass('active');
 	$(elem).parent().addClass('active');
 
-	$("#tabbar .near-selected").removeClass('near-selected');
+	tabbar.find(".near-selected").removeClass('near-selected');
 	if ($(elem).parent().hasClass('first')) {
-		$(".outer-tabbar-left").addClass('near-selected');
+		tabbar.find(".outer-tabbar-left").addClass('near-selected');
 	} else if($(elem).parent().hasClass('last')) {
-		$(".outer-tabbar-right").addClass('near-selected');
+		tabbar.find(".outer-tabbar-right").addClass('near-selected');
 	}
 
-	$("#tabbar .left-to-actived").removeClass('left-to-active');
-	$("#tabbar .right-to-actived").removeClass('right-to-active');
+	tabbar.find(".left-to-actived").removeClass('left-to-active');
+	tabbar.find(".right-to-actived").removeClass('right-to-active');
 	if ($(elem).parent().next().length)
 		$(elem).parent().next().addClass('right-to-active');
 	if ($(elem).parent().prev().length)
