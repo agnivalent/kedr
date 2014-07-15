5a322157fb502a8b8346c44155c62cd7c16ea1e4
Eugene <agnivalent@gmail.com>
2014-07-15 13:14:21 +0400

    mail

--- server.rb
+++ server.rb
@@ -2,13 +2,14 @@ require 'bundler/setup'
 require 'sinatra'
 require 'pony'
 
-get '/' do
-  "Hello world!"
-end
-
 post '/mailer' do
+# 	name-field:1
+# area-info:2
+# now:on
+# calendar:15.07.2014
+# area-info:3
   Pony.mail(:to => 'agnivalent@gmail.com',
             :from => 'no-reply@kedr.ru',
-            :subject => "Обратная связь: #{params[:name]} (+7 #{params[:phone]})",
+            :subject => "rerere",
             :body => params[:message])
 end
