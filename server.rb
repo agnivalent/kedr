a256608d09f8de0468b2b821ef16d557e744b014
Eugene <agnivalent@gmail.com>
2014-07-15 13:44:42 +0400

    mailer 02

--- server.rb
+++ server.rb
@@ -3,13 +3,25 @@ require 'sinatra'
 require 'pony'
 
 post '/mailer' do
-# 	name-field:1
-# area-info:2
+# name:тфьу
+# phone:234
 # now:on
 # calendar:15.07.2014
-# area-info:3
-  Pony.mail(:to => 'agnivalent@gmail.com',
+# message:йауц
+
+	message = "#{params[:name]} просит связаться по телефону #{params[:phone]} "
+		
+	message << params[:now].to_s == "on" ? "сейчас" : "#{params[:calendar]} в #{params[:time]}"
+	message << "\n"
+	message << "Сообщение: #{params[:message]}"
+	
+  	Pony.mail(:to => 'agnivalent@gmail.com',
             :from => 'no-reply@kedr.ru',
-            :subject => "rerere",
-            :body => params[:message])
+            :subject => "Обратная связь: #{params[:name]}",
+			:via => :sendmail,
+  			:via_options => {
+    			:location  => '/usr/sbin/sendmail', # defaults to 'which sendmail' or '/usr/sbin/sendmail' if 'which' fails
+    			:arguments => '-t' # -t and -i are the defaults
+  			},
+            :body => message)
 end
