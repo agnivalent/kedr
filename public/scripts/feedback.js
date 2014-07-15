d05f0bc22f224b30e67a465e7b796f3ffabc53cc
Eugene <agnivalent@gmail.com>
2014-07-15 14:20:48 +0400

    mailer 06

--- public/scripts/feedback.js
+++ public/scripts/feedback.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 $('.feedback-form form').submit(function () {
  	// sendContactForm();
- 	$.post('/api/mailer', $('.feedback-form form').serialize());
+ 	$.post('/api/mailer', $('.feedback-form form').serialize(), function() {
+ 		alert('Ваше сообщение было успешно послано.');
+ 	});
+
  	return false;
 });
 });
