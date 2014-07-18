14d63f62980f2d193fe186d33e66f7e166de3c24
Eugene <agnivalent@gmail.com>
2014-07-18 16:53:04 +0400

    target blank

---
+++ public/scripts/misc.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('a.smooth').click(function(){
+    	$('html, body').animate({
+        	scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    	}, 500);
+    return false;
+});
+});
