b0e2566292c09dabc83e653a9272efec05588757
iogr <gr0m@nm.ru>
2014-08-13 14:21:22 +0400

    required value for feedback

--- public/scripts/feedback.js
+++ public/scripts/feedback.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 $('.feedback-form form').submit(function () {
  	// sendContactForm();
+
+var value = document.getElementById('area-field').value;
+ if (value.length < 10) {
+ 	alert ('Пожалуйста, заполните поле с номером телефона.');
+   return false; // keep form from submitting
+ }
+ else {
+
+
  	$.post('/api/mailer', $('.feedback-form form').serialize(), function() {
  		alert('Ваше сообщение было успешно послано.');
  	});
 
+}
  	return false;
 });
 });
