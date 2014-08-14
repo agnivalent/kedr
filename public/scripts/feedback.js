3c2e6524a5470f352c01f2b6f747c41ebcb436d2
iogr <gr0m@nm.ru>
2014-08-14 15:52:38 +0400

    email field, minor fixes

--- public/scripts/feedback.js
+++ public/scripts/feedback.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 $('.feedback-form form').submit(function () {
  	// sendContactForm();
 
-var value = document.getElementById('area-field').value;
- if (value.length < 10) {
- 	alert ('Пожалуйста, заполните поле с номером телефона.');
+var value1 = document.getElementById('name-field').value;
+var value2 = document.getElementById('email-field').value;
+ if (value1.length < 1 || value2.length < 4) {
+ 	alert ('Пожалуйста, заполните поля с вашим именем и Email.');
    return false; // keep form from submitting
  }
  else {
