c7051a5be25dbea1a9377372219c6bda8f56419e
Eugene <agnivalent@gmail.com>
2014-08-20 16:04:59 +0400

    fixes in validation

--- public/scripts/feedback.js
+++ public/scripts/feedback.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
 		});
 $('.feedback-form form').submit(function () {
  	// sendContactForm();
- 	$.post('/api/mailer', $('.feedback-form form').serialize(), function() {
- 		alert('Ваше сообщение было успешно послано.');
- 	});
+ 	if($('.feedback-form form').valid()) {
+ 		$.post('/api/mailer', $('.feedback-form form').serialize(), function() {
+ 			alert('Ваше сообщение было успешно послано.');
+ 		});
+ 	}
 
  	return false;
 });
